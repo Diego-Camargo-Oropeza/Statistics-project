9ff1189916a74660879370e4b0bdd19d59d5c3cb
leilani <leilanivelasco1369@gmail.com>
2026-02-21 22:49:18 -0600

    Add files via upload
    incluye frecuencias y cambio en el ciclo

--- src/statisticsfirstmidterm/Interval.java
+++ src/statisticsfirstmidterm/Interval.java
@@ -1,11 +1,16 @@
+
 package statisticsfirstmidterm;
 
 public class Interval {
-
     private final double lowerLimit;
     private final double upperLimit;
     private final double classMark;
     private int frequency;
+    private int cumFrequency;
+    private double relFrequency;
+    private double cumRelFrequency;
+    
+   
 
     public Interval(double lowerLimit, double upperLimit) {
         this.lowerLimit = lowerLimit;
@@ -13,6 +18,9 @@ public class Interval {
         // Class mark (midpoint) is the representative value for grouped formulas.
         this.classMark = (lowerLimit + upperLimit) / 2.0;
         this.frequency = 0;
+        this.cumFrequency = 0;
+        this.relFrequency = 0.0;
+        this.cumRelFrequency = 0.0;
     }
 
     public double getLowerLimit() {
@@ -30,7 +38,31 @@ public class Interval {
     public int getFrequency() {
         return frequency;
     }
-
+    
+    public int getCumFrequency(){
+        return cumFrequency;
+    }
+    
+    public double getRelFrequency(){
+        return relFrequency;
+    }
+    
+    public double getCumRelFrequency(){
+        return cumRelFrequency;
+    }
+    
+    public void setCumFrequency(int cumFrequency){
+        this.cumFrequency = cumFrequency;
+    }
+    
+    public void setRelFrequency(int n){
+        this.relFrequency = (double)frequency / n;
+    }
+    
+    public void setCumRelFrequency(double cumRelFrequency){
+        this.cumRelFrequency = cumRelFrequency;
+    }
+    
     public void incrementFrequency() {
         // Count how many sample values fall inside this interval.
         frequency++;
